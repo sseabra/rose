603cd99e2ae97bc26357f3566d522388197d26c6
Sérgio Seabra <sseabra@redhat.com>
2017-12-12 15:46:40 +0000

    Update index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
         <div class="col-md-6 col-md-offset-3">
           <h2>Application Example</h2>
           <p>
-            <img src="edp_marq.jpg">
+            <img src="edp_24.jpg">
           </p>
         </div>
       </div>
